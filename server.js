57517fd7f7c57ae5d630bc9fee4801fd52a6a07f
sergygordeev95@gmail.com <sergygordeev95@gmail.com>
2017-04-11 09:16:03 +0400

    asdas

---
+++ server.js
@@ -0,0 +1,57 @@
+const express  = require('express')
+//const db = require('./db')
+
+//const rotesApi = require('./routes')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+const router = express.Router()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', express.static( __dirname ))
+app.use('/api', router)
+
+const process = require('process')
+
+const port = process.env.PORT || 8788
+
+app.listen(port, function () {
+	console.log(`Start server on: ${port}`)
+})
+
+app.on('error', err =>{
+	console.log('Server error')
+	throw err
+});
+
+router.post('/auth/login',(req,res)=>{
+	console.log(req.body)
+	request.post({url:'http://psyouth.herokuapp.com/api/auth/login', form: req.body }, function (error, response, body) {
+		let result = JSON.parse(response.body)
+		if(result.message){
+			res.status(404)
+		}
+		res.json(result)
+	})
+})
+
+module.exports = app
+
+let staticRouts = 
+[
+'auth',
+'sign_in',
+'sign_up',
+'tests',
+'admin'
+]
+
+initStaticRouts(staticRouts)
+function initStaticRouts(routs){
+	routs.forEach(route =>{
+		app.use(`/${route}*`,express.static(__dirname))
+	})
+}
